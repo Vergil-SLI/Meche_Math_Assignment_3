95c8801464570081ccf7c6b574a4926a1d527e98
Aqazilbash <157313524+Aqazilbash@users.noreply.github.com>
2025-10-10 15:43:44 -0400

    implicit midpoint

--- call_functions.m
+++ call_functions.m
@@ -1,8 +1,8 @@
 %truncation_error(0)
 
-[t_list, x_list, h_avg, num_evals] = backward_euler(@rate_func01, [0,(pi/4)], 1, 0.01)
+[t_list, x_list, h_avg, num_evals] = implicit_midpoint(@rate_func01, [0,(pi/4)], 1, 0.01)
 plot(t_list,x_list); hold on
-plot(t_list, solution01(t_list))
+    plot(t_list, solution01(t_list))
 
 function dXdt = rate_func01(t,X)
     dXdt = -5*X + 5*cos(t) - sin(t);
