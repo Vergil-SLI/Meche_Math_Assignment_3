77e8cd9f9f9b1f8152c995d345e2b68fbccb538e
Aqazilbash <157313524+Aqazilbash@users.noreply.github.com>
2025-10-12 15:03:23 -0400

    graphing

--- truncation_error.m
+++ truncation_error.m
@@ -91,7 +91,31 @@ function truncation_error(mode)
     % _____________________________________________________________________
     %comparing implicit midpoint with closed form solution
     if mode == 4
-       
+       % Comparing closed form approximation (of various time step) with numerical solutions
+        % Implicit Midpoint
+        h = linspace(0.375, 0.475, 3); % WHAT ARE GOOD H VALUES
+        t_start = 0;
+        t_end = 7*pi/4;
+        legend_titles = cell(1, length(h));
+        legend_titles{1, 1} = "numerical solution";
+
+        % plot the numerical solution
+        hold off
+        t_list = linspace(t_start, t_end, 20);
+        numerical_X_list = solution01(t_list);
+        plot(t_list, numerical_X_list, 'black')
+        hold on
+
+        % plot the approximations (of various time step)
+        for i = 1:length(h)
+            [t_list, foward_euler_X_list, h_avg, ~] = implicit_midpoint(@rate_func01,[t_start, t_end],solution01(t_start), h(i));
+            plot(t_list, foward_euler_X_list, "-")
+            legend_titles{1, i+1} = "closed form approx (h = " + num2str(round(h_avg, 3)) + ")";
+        end
+        lgd = legend(legend_titles);
+        lgd.Location = "southeast";
+        xlabel("t")
+        ylabel("X Value")
     end
 
     % _____________________________________________________________________
@@ -102,15 +126,19 @@ function truncation_error(mode)
         h = 0.38;
         [t_list, foward_euler_X_list, ~, ~] = forward_euler(@rate_func01,[t_start, t_end],solution01(t_start), h);
         [~, explicit_midpoint_X_list, ~, ~] = explicit_midpoint(@rate_func01,[t_start, t_end],solution01(t_start), h);
+        [~, backward_euler_X_list, ~, ~] = backward_euler(@rate_func01,[t_start, t_end],solution01(t_start), h);
+        [~, implicit_midpoint_X_list, ~, ~] = implicit_midpoint(@rate_func01,[t_start, t_end],solution01(t_start), h);
         numerical_X_list = solution01(t_list);
 
         hold off
         plot(t_list, foward_euler_X_list, 'b-')
         hold on
         plot(t_list,explicit_midpoint_X_list, 'r-')
+        plot(t_list,backward_euler_X_list, 'y-')
+        plot(t_list,implicit_midpoint_X_list, 'm-')
         plot(t_list, numerical_X_list, 'g-')
 
-        lgd = legend('forward euler', 'explicit midpoint', 'numerical solution'); % need the 2 implicit ones still
+        lgd = legend('forward euler', 'explicit midpoint', 'backwards euler', 'implicit midpoint', 'numerical solution');
         lgd.Location = "southeast";
         xlabel("t")
         ylabel("X Value")
@@ -159,7 +187,46 @@ function truncation_error(mode)
     
     % local truncation of all 4 methods
     if mode == 7
+        % Local truncation error between the 2 explicit methods
+        t0 = pi/6;
+        h = logspace(-5, 1, 100);
+        analytical_difference = zeros(1, length(h));
+        exp_euler_local_error = zeros(1, length(h));
+        exp_midpoint_local_error = zeros(1, length(h));
+        implicit_euler_local_error = zeros(1, length(h));
+        imp_midpoint_local_error = zeros(1, length(h));
+        h_avg = zeros(1, length(h));
 
+        for i = 1:length(h)
+            analytical_difference(i) = abs(solution01(t0+h(1,i))-solution01(t0));
+            [exp_euler_local_error(i), ~, ~] = forward_euler_local_error(t0, h(1,i));
+            [exp_midpoint_local_error(i), h_avg_temp, ~] = explicit_midpoint_local_error(t0, h(1,i));
+            [implicit_euler_local_error(i), ~, ~] = backward_euler_local_error(t0, h(1,i));
+
+
+            h_avg(1, i) = h_avg_temp;
+        end
+
+        [p1,k1] = loglog_fit(h,analytical_difference);
+        [p2,k2] = loglog_fit(h_avg,exp_euler_local_error);
+        [p3,k3] = loglog_fit(h_avg,exp_midpoint_local_error);
+
+        % plot the local truncation error
+        hold off
+        loglog(h,analytical_difference, 'b.', MarkerSize=10)
+        hold on
+        loglog(h_avg,imp_euler_local_error, 'r.', MarkerSize=10)
+        loglog(h_avg,exp_midpoint_local_error, 'g.', MarkerSize=10)
+        loglog(h, k1*(h.^p1), 'b-')
+        loglog(h_avg, k2*(h_avg.^p2), 'r-')
+        loglog(h_avg, k3*(h_avg.^p3), 'g-')
+        xlabel("Average timestep length h")
+        ylabel("Error")
+        lgd = legend("analytical difference","forward euler local error", "explicit midpoint local error",...
+            "k = " + k1 + ", p = " + p1,...
+            "k = " + k2 + ", p = " + p2,...
+            "k = " + k3 + ", p = " + p3);
+        lgd.Location = "southeast";
     end
 
     % % _____________________________________________________________________
@@ -217,6 +284,26 @@ function [local_error, h_avg, num_evals] = explicit_midpoint_local_error(t, h)
     % calculate local error for the first test function
     
     [~,X_list, h_avg, num_evals] = explicit_midpoint(@rate_func01,[t, t+h],solution01(t),h+1);
+    
+    G_t = X_list(end);
+    X_t = solution01(t+h);
+
+    local_error = norm(G_t - X_t);
+end
+
+function [local_error, h_avg, num_evals] = backward_euler_local_error(t, h)
+    % calculate local error for the first test function
+    [~,X_list,h_avg, num_evals] = backward_euler(@rate_func01,[t, t+h],solution01(t),h+1);
+    G_t = X_list(end);
+    X_t = solution01(t+h);
+
+    local_error = norm(G_t - X_t);
+end
+
+function [local_error, h_avg, num_evals] = implicit_midpoint_local_error(t, h)
+    % calculate local error for the first test function
+    
+    [~,X_list, h_avg, num_evals] = implicit_midpoint(@rate_func01,[t, t+h],solution01(t),h+1);
     
     G_t = X_list(end);
     X_t = solution01(t+h);
